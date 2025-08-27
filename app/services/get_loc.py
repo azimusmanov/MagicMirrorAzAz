0abb46fb16e21925808888ac65e8b0f6d5b444b0
Azim's MacBook <buchkirex@gmail.com>
2025-08-26 22:24:11 -0400

    added timezone function

--- app/services/get_loc.py
+++ app/services/get_loc.py
@@ -1,5 +1,6 @@
 import json
 import urllib.request
+from timezonefinder import TimezoneFinder
 
 def get_location():
     try:
@@ -12,8 +13,15 @@ def get_location():
     except Exception as e:
         print("Location error:", e)
         return None, None
+    
+def get_timezone_from_coords(lat, lon):
+    tf = TimezoneFinder()
+    tzname = tf.timezone_at(lat=lat, lng=lon)  # e.g., "America/Aruba"
+    return tzname
 
 if __name__ == "__main__":
     lat, lon = get_location()
+    tzname = get_timezone_from_coords(lat, lon)
     print("Latitude:", lat)
     print("Longitude:", lon)
+    print("Timezone: ", tzname)
